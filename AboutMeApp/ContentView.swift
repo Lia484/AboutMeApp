dafab8849e25c2b5480d6f9733bd835f69eb0042
Lia <lworland28@stjoesphacademy.org>
2025-06-04 13:57:40 -0500

    have me pic and bday/name/family text

--- AboutMeApp/ContentView.swift
+++ AboutMeApp/ContentView.swift
@@ -10,10 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello!")
+            Image("me")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+            Text("Lia Worland")
+                .font(.title)
+                .fontWeight(.regular)
+                .foregroundColor(Color.black)
+            Text("Dec. 30, 2009")
+                .font(.title)
+                .fontWeight(.regular)
+                .foregroundColor(Color.black)
+            HStack {
+                Text("Family")
+                    .font(.title)
+                    .fontWeight(.regular)
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.leading)
+                    .padding(.bottom, 400.0)
+                Spacer()
+                Image("")
+            }
         }
         .padding()
     }
